e102d814c2c09ae4f0eb338729bf7ace1b098ea5
chipslays <hello@chipslays.ru>
2021-01-26 13:35:23 +0400

    Add project files.

---
+++ ModMenu/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace ModMenu
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Tick += onTick;
+            KeyDown += onKeyDown;
+        }
+        private void onTick(object sender, EventArgs e)
+        {
+
+        }
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                UI.Notify("Hello World");
+            }
+        }
+    }
+}
